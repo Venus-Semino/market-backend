dac974310a06f35cb69b467694187104d1ea6e29
Semino Alemán Venus Getsemaní <venus.semino@tecdesoftware>
2026-07-13 07:58:39 -0600

    feat(purchase): debuggin

---
+++ src/main/java/mx/edu/tecdesoftware/market_backend/domain/repository/PurchaseRepository.java
@@ -0,0 +1,11 @@
+package mx.edu.tecdesoftware.market_backend.domain.repository;
+
+import mx.edu.tecdesoftware.market_backend.domain.Purchase;
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
---
+++ src/main/java/mx/edu/tecdesoftware/market_backend/persistence/CompraRepository.java
@@ -0,0 +1,44 @@
+package mx.edu.tecdesoftware.market_backend.persistence;
+
+import mx.edu.tecdesoftware.market_backend.domain.Purchase;
+import mx.edu.tecdesoftware.market_backend.domain.repository.PurchaseRepository;
+import mx.edu.tecdesoftware.market_backend.persistence.crud.PurchaseCrudRepository;
+import mx.edu.tecdesoftware.market_backend.persistence.entity.Compra;
+import mx.edu.tecdesoftware.market_backend.persistence.mapper.PurchaseMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CompraRepository implements PurchaseRepository {
+
+    @Autowired
+    private PurchaseCrudRepository purchaseCrudRepository;
+
+    @Autowired
+    private PurchaseMapper mapper;
+
+    @Override
+    public List<Purchase> getAll() {
+        return mapper.toPurchases((List<Compra>) purchaseCrudRepository.findAll());
+    }
+
+    @Override
+    public Optional<List<Purchase>> getByClient(String clientId) {
+        return purchaseCrudRepository.findByIdCliente(clientId)
+                .map(compras -> mapper.toPurchases(compras));
+    }
+
+    @Override
+    public Purchase save(Purchase purchase) {
+        Compra compra = mapper.toCompra(purchase);
+
+        if (compra.getProductos() != null) {
+            compra.getProductos().forEach(producto -> producto.setCompra(compra));
+        }
+
+        return mapper.toPurchase(purchaseCrudRepository.save(compra));
+    }
+}
